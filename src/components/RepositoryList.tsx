02c68bb94c1b51c35e1923614233c6b6ddf4357e
tondinizr <welinton.dr@example.com>
2022-07-26 08:08:04 -0300

    Finalizacao aula 1

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repository.scss";
+
+interface Repository {
+    id: string,
+    name: string,
+    description: string,
+    html_url: string
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/users/tondinizr/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.id} repository={repository}/>}
+                )}
+            </ul>
+        </section>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App'
 
 const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+const root = createRoot(container!);
+root.render(<App />);
